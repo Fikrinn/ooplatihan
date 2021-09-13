e0b79aaa640ce0a5fa476abd0b957c448ded84d7
Fikrinn <fikrinurfaizi120@gmail.com>
2021-09-13 10:09:31 +0700

    oop

---
+++ oop2.php
@@ -0,0 +1,23 @@
+<?php 
+ class ucapan
+ {
+     public function siang()
+     {
+         return "Hai selamat siang";
+     }
+
+     public function sore()
+     {
+         return "Hai selamat sore";
+     }
+
+     public function __construct()
+     {
+         echo "Selamat pagi <br>";
+     }
+ }
+
+ $selamat = new Ucapan();
+ echo $selamat->siang() . "<br>";
+ echo $selamat->sore();
+?>
